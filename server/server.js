5475ac0ef976506741c72eaabc39f7426b728d88
toni <toni.margan@gmail.com>
2018-04-30 12:47:29 +0200

    init commit fix

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const path = require('path');
 const express = require('express');
 
-const port = procces.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 var app = express();
 var publicPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicPath));
 
-app.listen(port, ()=>{
-  console.log(`Server started on port ${port}`);
-})
+app.listen(port, () => {
+  console.log("Server started");
+});
