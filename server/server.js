c64fb92acb4fc91f8c62d70e4c8aa70ea9861041
toni <toni.margan@gmail.com>
2018-05-14 11:28:43 +0200

    add new message and create message events

--- server/server.js
+++ server/server.js
@@ -13,12 +13,21 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) =>{
+  console.log('New user connected');
 
-      console.log('New user connected');
+  socket.emit('newMessage', {
+    from: 'pero',
+    text: 'hey sup',
+    createdAt: 123
+  });
 
-      socket.on('disconnect' , () =>{
-        console.log('User disconnected.');
-      });
+  socket.on('disconnect' , () => {
+    console.log('User disconnected.');
+  });
+
+  socket.on('createMessage' , (newMessage) => {
+    console.log('New message.', newMessage);
+  });
 
 });
 
