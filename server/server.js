efc241423984a763d48d537b2788f75fbf2784b7
toni <toni.margan@gmail.com>
2018-05-14 11:02:28 +0200

    Socket init

--- server/server.js
+++ server/server.js
@@ -1,12 +1,27 @@
 const path = require('path');
+const http = require('http');
+
 const express = require('express');
+const socketIO = require('socket.io');
 
 const port = process.env.PORT || 3000;
-var app = express();
 var publicPath = path.join(__dirname, '../public');
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) =>{
+
+      console.log('New user connected');
+
+      socket.on('disconnect' , () =>{
+        console.log('User disconnected.');
+      });
+
+});
+
+server.listen(port, () => {
   console.log("Server started");
 });
