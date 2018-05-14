0aa2306408300db111df3413af8b7d795442c665
toni <toni.margan@gmail.com>
2018-05-14 12:23:39 +0200

    greeting users

--- server/server.js
+++ server/server.js
@@ -15,6 +15,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) =>{
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    created: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+      from: 'Admin',
+      text: 'New user joined',
+      created: new Date().getTime()
+  });
 
   socket.on('disconnect' , () => {
     console.log('User disconnected.');
@@ -26,7 +37,12 @@ io.on('connection', (socket) =>{
       from: newMessage.from,
       text: newMessage.text,
       created: new Date().getTime()
-    })
+    });
+    // socket.broadcast.emit('newMessage', {
+    //     from: newMessage.from,
+    //     text: newMessage.text,
+    //     created: new Date().getTime()
+    // });
   });
 
 });
