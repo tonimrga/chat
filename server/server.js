741a31e58290dfb73c18d9ad960e9be2565ee365
toni <toni.margan@gmail.com>
2018-05-15 10:43:20 +0200

    adding jquerry and form

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-io.on('connection', (socket) =>{
+io.on('connection', (socket) => {
   console.log('New user connected');
 
   socket.emit('newMessage', generateMessage('Admin', 'Welcome to chat'));
@@ -25,9 +25,10 @@ io.on('connection', (socket) =>{
     console.log('User disconnected.');
   });
 
-  socket.on('createMessage' , (newMessage) => {
+  socket.on('createMessage' , (newMessage, callback) => {
     console.log('New message.', newMessage);
     io.emit('newMessage', generateMessage(newMessage.from, newMessage.text));
+    callback('This is from the server');
     // socket.broadcast.emit('newMessage', {
     //     from: newMessage.from,
     //     text: newMessage.text,
