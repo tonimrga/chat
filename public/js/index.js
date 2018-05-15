5b45eb5f496f960423b5b900ef57955709532b40
toni <toni.margan@gmail.com>
2018-05-15 14:19:06 +0200

    adding styles

--- public/js/index.js
+++ public/js/index.js
@@ -36,7 +36,7 @@ jQuery('#message-form').on('submit', function(e){
     from: 'User: ',
     text: jQuery('[name=message]').val()
   }, function () {
-
+    jQuery('[name=message]').val('')
   })
 });
 
@@ -47,12 +47,17 @@ locationButton.on('click', function(){
         return alert('Geolocation not supported by your browser.')
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function(position) {
+      locationButton.removeAttr('disabled').text("Send location");
+
       socket.emit('createLocation', {
          latitude: position.coords.latitude,
          longitude: position.coords.longitude
       });
     }, function () {
+      locationButton.removeAttr('disabled').text("Send location");
       alert('Unable to fetch location.');
     });
 });
