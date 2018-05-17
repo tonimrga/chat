83cf317ca2bed00bd2fada734790b8a0911ccb94
toni <toni.margan@gmail.com>
2018-05-17 12:24:19 +0200

    add moustache.js

--- public/js/index.js
+++ public/js/index.js
@@ -8,24 +8,33 @@ socket.on('disconnect', function() {
   console.log('Disconnected from server.');
 });
 
+
 socket.on('newMessage', function(message) {
   var formattedTime = moment(message.created).format('HH:mm');
-  var li = jQuery('<li></li>');
-  li.text(`${formattedTime} ${message.from}: ${message.text}`);
+  var template = jQuery('#message-template').html();
 
-  jQuery('#poruke').append(li);
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    created: formattedTime
+  });
+
+  jQuery('#poruke').append(html);
+
 });
 
 socket.on('newMessageLocation', function(message) {
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My current location</a>');
+    var formattedTime = moment(message.created).format('HH:mm');
+  var template = jQuery('#location-message-template').html();
 
-  var formattedTime = moment(message.created).format('HH:mm');
-  li.text(`${formattedTime} ${message.from}: `);
-  a.attr('href', message.url);
-  li.append(a);
+  var html = Mustache.render(template, {
+    url: message.url,
+    from: message.from,
+    created: formattedTime
+  });
 
-  jQuery('#poruke').append(li);
+  jQuery('#poruke').append(html);
+
 });
 
 
